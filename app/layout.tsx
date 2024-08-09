25150c9e7904bc01e646f9291e8ce4d9bc083c63
phucthinhhh <tonilo2k4@gmail.com>
2024-08-09 15:21:21 +0700

    Authentication-Clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 
 import { cn } from "@/lib/utils"
 import { Metadata } from "next"
+import { ClerkProvider } from "@clerk/clerk-react"
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -17,16 +18,24 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: {children: React.ReactNode}) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <ClerkProvider appearance={{
+      baseTheme : 'dark',
+      variables : { 
+        colorPrimary : "#3371FF",
+        fontSize : '16px', 
+      },
+    }}>
+      <html lang="en" suppressHydrationWarning>
 
-      <body
-        className={cn(
-          "min-h-screen font-sans antialiased",
-          fontSans.variable
-        )}
-      >
-        {children}
-      </body>
-    </html>
+        <body
+          className={cn(
+            "min-h-screen font-sans antialiased",
+            fontSans.variable
+          )}
+        >
+          {children}
+        </body>
+      </html>
+      </ClerkProvider>
   )
 }
